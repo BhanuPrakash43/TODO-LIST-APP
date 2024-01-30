a9f219b5a66cec11b2b14b9bc569b17379923640
BhanuPrakash43 <bhanu742630@gmail.com>
2024-01-31 02:38:09 +0530

    spelling error fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,15 +6,15 @@ function About() {
       <h1>About Todo app</h1>
       <div>
       <h2>How Todo app works?</h2>
-      <h3>ANS - You can type your work list in the input box and just clck enter or add button.</h3>
+      <h3>ANS - You can type your work list in the input box and just click enter or add button to add your todos.</h3>
       </div>
       <div>
       <h2>What are the functions of this app?</h2>
-      <h3>ANS - Major functons of this app are after adding your todos, you can also update, delete and marks as completed your todos.</h3>
+      <h3>ANS - Major functons of this app are after adding your todos, you can update, delete and mark as completed your todos.</h3>
       </div>
       <div>
       <h2>How completed todos works?</h2>
-      <h3>ANS - Inside you completed todos, your all completed todos will be displayed which you mark as completed on the home page and there it will show your todo is crossed.</h3>
+      <h3>ANS - Inside completed todos, your all completed todos will be displayed which you mark as completed on the home page and there it will show your todo is crossed.</h3>
       </div>
       <div>
       <h2>What are the other functions?</h2>
