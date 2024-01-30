8b642edcc057f8f3731ef066990a4d0ecdf3ffda
BhanuPrakash43 <bhanu742630@gmail.com>
2024-01-31 02:34:54 +0530

    functions of about section added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,26 @@
+import styles from "../pages/About.module.css";
+
 function About() {
   return (
-    <h1>This is About page</h1>
+    <div className={styles.about}>
+      <h1>About Todo app</h1>
+      <div>
+      <h2>How Todo app works?</h2>
+      <h3>ANS - You can type your work list in the input box and just clck enter or add button.</h3>
+      </div>
+      <div>
+      <h2>What are the functions of this app?</h2>
+      <h3>ANS - Major functons of this app are after adding your todos, you can also update, delete and marks as completed your todos.</h3>
+      </div>
+      <div>
+      <h2>How completed todos works?</h2>
+      <h3>ANS - Inside you completed todos, your all completed todos will be displayed which you mark as completed on the home page and there it will show your todo is crossed.</h3>
+      </div>
+      <div>
+      <h2>What are the other functions?</h2>
+      <h3>ANS - When you click on the clear all todos button, it will delete all the existing todos.</h3>
+      </div>
+    </div>
   )
 }
 export default About
